f3f51911c28a18520c4558dd39419c91433ff19f
Edesaine <132795392+Edesaine@users.noreply.github.com>
2024-04-19 17:27:37 +0700

    Update admin

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,41 @@ Route::get('/layout', function () {
     return view('Customer.Layout.index');
 })->name('layout_page');
 
-Route::get('author',[\App\Http\Controllers\AuthorController::class,'index']);
+Route::get('author/index',[\App\Http\Controllers\AuthorController::class,'index']);
 Route::get('author/create',[\App\Http\Controllers\AuthorController::class,'create']);
 Route::post('author/create',[\App\Http\Controllers\AuthorController::class,'store']);
 Route::get('author/{id}/edit',[\App\Http\Controllers\AuthorController::class,'edit']);
 Route::put('author/{id}/edit',[\App\Http\Controllers\AuthorController::class,'update']);
 Route::get('author/{id}/delete',[\App\Http\Controllers\AuthorController::class,'delete']);
 
-Route::get('publisher',[\App\Http\Controllers\PublisherController::class,'index']);
+Route::get('publisher/index',[\App\Http\Controllers\PublisherController::class,'index']);
 Route::get('publisher/create',[\App\Http\Controllers\PublisherController::class,'create']);
 Route::post('publisher/create',[\App\Http\Controllers\PublisherController::class,'store']);
 Route::get('publisher/{id}/edit',[\App\Http\Controllers\PublisherController::class,'edit']);
 Route::put('publisher/{id}/edit',[\App\Http\Controllers\PublisherController::class,'update']);
 Route::get('publisher/{id}/delete',[\App\Http\Controllers\PublisherController::class,'delete']);
 
-Route::get('customer',[\App\Http\Controllers\CustomerController::class,'index']);
-Route::post('customer',[\App\Http\Controllers\CustomerController::class,'store']);
+Route::get('customer/index',[\App\Http\Controllers\CustomerController::class,'index']);
+Route::post('customer/index',[\App\Http\Controllers\CustomerController::class,'store']);
+
+Route::get('book/index',[\App\Http\Controllers\BookController::class,'index']);
+Route::get('book/create',[\App\Http\Controllers\BookController::class,'create']);
+Route::post('book/create',[\App\Http\Controllers\BookController::class,'store']);
+Route::get('book/{id}/edit',[\App\Http\Controllers\BookController::class,'edit']);
+Route::put('book/{id}/edit',[\App\Http\Controllers\BookController::class,'update']);
+Route::get('book/{id}/delete',[\App\Http\Controllers\BookController::class,'delete']);
 
 Route::middleware(LoginCheckingCustomer::class)->group(function () {
     Route::get('/profile', [CustomerController::class, 'editProfile'])->name('profile');
     Route::put('/profile', [CustomerController::class, 'updateProfile'])->name('profile.update');
 });
+
+Route::get('/register', [CustomerController::class, 'register'])->name('Customer.account.register');
+Route::post('/register', [CustomerController::class, 'registerProcess'])->name('Customer.account.registerProcess');
+
+Route::get('/login', [CustomerController::class, 'login'])->name('Customer.account.login');
+Route::post('/login', [CustomerController::class, 'loginProcess'])->name('Customer.account.loginProcess');
+
 
 Route::get('/register', [CustomerController::class, 'register'])->name('Customer.account.register');
 Route::post('/register', [CustomerController::class, 'registerProcess'])->name('Customer.account.registerProcess');
