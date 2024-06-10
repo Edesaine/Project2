01e28c12df8bf071c7030d1a52a01a27d4f98fda
Edesaine <132795392+Edesaine@users.noreply.github.com>
2024-06-10 15:14:00 +0700

    Update HomeController.php for search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\Customer;
 use App\Requests;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
@@ -15,5 +16,16 @@ class HomeController extends Controller
         $books = Book::take(9)->get();
         $sach = Book::orderBy("price","desc")->first();
         return view('Customer.Layout.index', compact('books','customer','sach'));
+    }
+    public function search(Request $request)
+    {
+        $search="";
+        if($request->search){
+            $search=$request->search;
+        }
+        $customer = Auth::guard('customer')->user();
+        $books = Book::where('books.name', 'like', '%' . $search . '%')
+            ->get();
+        return view('Customer.Layout.search', compact('books','customer','search'));
     }
 }
